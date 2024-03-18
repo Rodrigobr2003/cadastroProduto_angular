f16217302c458324b2e993bb8a5ed5546d83a248
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-03-18 16:29:46 -0300

    Add bootstrap/HTML/CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CadastroPrdutosComponent } from './cadastro-prdutos/cadastro-prdutos.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CadastroPrdutosComponent } from './cadastro-prdutos/cadastro-prdutos.co
     CadastroPrdutosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
